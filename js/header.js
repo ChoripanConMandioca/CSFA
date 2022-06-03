f51f8c71b9ffa0998590bba838ebe3d502b9478c
Refreskaii <jemo1909@gmail.com>
2022-06-03 11:53:57 -0400

    HAGAN CARAJO

---
+++ js/header.js
@@ -0,0 +1,14 @@
+let appHeader = `<div class="logo">
+<a href="index.html"><img src="/img/logo.png" alt="Logo del colegio San Francisco de Asís" class="logo-colegio"></a>
+</div>
+<nav> 
+    <ul class="secondary-nav">
+        <li><a href="index.html">Inicio</a></li>
+        <li><a href="sobre_nosotros.html">Sobre Nosotros</a></li>
+        <li><a href="niveles.html">Niveles</a></li>
+        <li><a href="noticias.html">Noticias</a></li>
+        <li><a href="contacto.html">Contacto</a></li>
+    </ul>
+</nav>`
+
+document.getElementById("app-header").innerHTML = appHeader;
