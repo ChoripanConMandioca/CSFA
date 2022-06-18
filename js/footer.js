ead8402e6a77d9bd418752428d8818bb7ce849e2
ChoripanConMandioca <jemo1909@gmail.com>
2022-06-17 22:30:04 -0400

    me quiero morir

---
+++ js/footer.js
@@ -0,0 +1,42 @@
+let appFooter = `<div class="grupo-1">
+<div class="box">
+    <figure>
+        <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d1802.5390155765394!2d-57.58520922931319!3d-25.368701161500173!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x7b0535d2c2784f25!2sColegio%20San%20Francisco!5e0!3m2!1ses-419!2spy!4v1655478356334!5m2!1ses-419!2spy" width="250" height="230px" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+    </figure>
+</div>
+<div class="box">
+    <ul>
+        <li><img src="/img/ico/phone.svg" alt="Teléfono" height="40px"></li>
+        <li> <h4>Llámanos </h4><p>+555 654 545</p></li> <br>
+        <li><img src="/img/ico/clock.svg" alt="Horario" height="40px"></li>
+        <li><h4>Horario </h4><p>Lun - Vie 7AM - 5PM</p></li><br>
+        <li><img src="/img/ico/marker.svg" alt="Dirección" height="40px"></li>
+        <li><h4>Dirección </h4><p>Av. Bon Poleski, Villa Elisa</p></li>
+    </ul>
+
+</div>
+<div class="box">
+    <h4>Acceso Rápido</h4>
+    <ol>
+        <li><a href="/html/index.html">Inicio</a></li> 
+        <li><a href="/html/sobre_nosotros.html">Sobre Nosotros</a></li>
+        <li><a href="/html/niveles.html">Niveles</a></li>
+        <li><a href="/html/noticias.html">Noticias</a></li>
+        <li><a href="/html/contacto.html">Contacto</a></li>
+    <ol>
+</div>
+<div class="box">
+    <div class="redsocial">
+        <ol>
+            <li style="margin-bottom: 35px;"><a href="#" ><img src="/img/ico/facebook-icon.svg" alt="Facebook" height="50px"></a></li>
+            <li><a href="#"><img src="/img/ico/instagram-icon.svg" alt="Instagram" height="50px"></a></li>
+        </ol>
+    </div>
+</div>
+</div>
+<div class="grupo-2" >
+<hr>
+<small>&copy; 2022 Colegio San Francsico de Asís | Paraguay</small>
+</div>`
+
+document.getElementById("app-footer").innerHTML = appFooter;
--- js/header.js
+++ js/header.js
@@ -1,13 +1,13 @@
 let appHeader = `<div class="logo">
 <a href="index.html"><img src="/img/logo.png" alt="Logo del colegio San Francisco de Asís" class="logo-colegio"></a>
 </div>
-<nav> 
+<nav class="navbar"> 
     <ul class="secondary-nav">
-        <li><a href="index.html">Inicio</a></li>
-        <li><a href="sobre_nosotros.html">Sobre Nosotros</a></li>
-        <li><a href="niveles.html">Niveles</a></li>
-        <li><a href="noticias.html">Noticias</a></li>
-        <li><a href="contacto.html">Contacto</a></li>
+        <li class="navbar-item"><a href="index.html">Inicio</a></li>
+        <li class="navbar-item"><a href="sobre_nosotros.html">Sobre Nosotros</a></li>
+        <li class="navbar-item"><a href="niveles.html">Niveles</a></li>
+        <li class="navbar-item"><a href="noticias.html">Noticias</a></li>
+        <li class="navbar-item"><a href="contacto.html">Contacto</a></li>
     </ul>
 </nav>`
 
